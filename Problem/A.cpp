d1f536b0472abb933abf2dc0a5d4f21ef05c6e25
yu <914179030@qq.com>
2018-08-07 10:04:35 +0800

    Hungarian KM

--- Problem/A.cpp
+++ Problem/A.cpp
@@ -1,17 +1,73 @@
 //============================================================================
-//Name：牛客多校第六场 A Singing Contest 模拟
+//Name：KM 带权二分图最佳匹配
 //============================================================================
 #include<bits/stdc++.h>
 
 #define IO ios_base::sync_with_stdio(0),cin.tie(0)
 #define rep(i, a, n) for (int i = a; i < n; i++)
+#define mm(arr, val) memset(arr, val, sizeof(arr))
 
 using namespace std;
-typedef long long LL;
 
-priority_queue<int, vector<int>, greater<int> > Q[1026 * 16 + 5];
-set<int> s[1026 * 16 + 5];
-queue<int> ans;
+const int N = 105;
+const int MAX = INT_MAX;
+const int MIN = INT_MIN;
+int n, m, minz, ans, arr[N][N];
+int cx[N], cy[N], wx[N], wy[N];
+bool visx[N], visy[N];
+
+//匈牙利算法找增广路径
+bool dfs(int s) {
+    visx[s] = 1;
+    for (int i = 1; i <= m; i++)
+        if (!visy[i]) {
+            int t = wx[s] + wy[i] - arr[s][i];
+//            相等子图中寻找增广路
+            if (t == 0) {
+                printf("(%d -- %d -- %d)\n", s, i, cy[i]);
+                visy[i] = 1;
+                if (cy[i] == 0 || dfs(cy[i])) {
+                    cx[s] = i, cy[i] = s;
+                    return true;
+                }
+            } else if (t > 0 && t < minz) {
+                //取最小的不在相等子图中的常数 d
+                minz = t;
+            }
+        }
+    return false;
+}
+
+void km() {
+    memset(cx, 0, sizeof(cx));
+    memset(cy, 0, sizeof(cy));
+    memset(wy, 0, sizeof(wy));
+    for (int i = 1; i <= n; i++) {
+        printf("%d start: \n", i);
+        while (1) {
+            minz = MAX;
+            memset(visx, 0, sizeof visx);
+            memset(visy, 0, sizeof visy);
+            if (dfs(i))break;
+
+            //将增广轨中 X 部的点的顶标减去 minz
+            for (int j = 1; j <= n; j++)
+                if (visx[j]) wx[j] -= minz;
+            //将增广轨中 Y 部的点的顶标加上 minz
+            for (int j = 1; j <= m; j++)
+                if (visy[j]) wy[j] += minz;
+
+            printf("min[%d] = %d\n", i, minz);
+            rep(j, 1, n + 1) {
+                printf("wx[%d] = %d  wy[%d] = %d\n", j, wx[j], j, wy[j]);
+            }
+        }
+    }
+
+    ans = 0;
+    for (int i = 1; i <= m; i++)
+        if (cy[i]) ans += arr[cy[i]][i];
+}
 
 int main() {
     IO;
@@ -20,37 +76,24 @@ int main() {
     freopen(R"(C:\Users\ACM-PC\CLionProjects\Competitaon\Problem\in)", "r", stdin);
     freopen(R"(C:\Users\ACM-PC\CLionProjects\Competitaon\Problem\out)", "w", stdout);
 #endif
-    int t, cnt = 0;
-    cin >> t;
-    while (t--) {
-        int n, N, x;
-        while (!ans.empty()) {
-            ans.pop();
-        }
-        cin >> n;
-        N = static_cast<int>(pow(2, n));
-        rep(i, 0, N) {
-            s[i].clear();
-            rep(j, 0, n) {
-                cin >> x;
-                s[i].insert(x);
-            }
-            ans.push(i);
-        }
+    cin >> n >> m;
 
-        while (ans.size() > 1) {
-            int p = ans.front();
-            ans.pop();
-            int q = ans.front();
-            ans.pop();
-            if (*(--s[p].end()) < *(--s[q].end())) {
-                swap(p, q);
-            }
-            s[p].erase(s[p].lower_bound(*(--s[q].end())));
-            ans.push(p);
+    rep(i, 1, n + 1) {
+        wx[i] = MIN;
+        rep(j, 1, m + 1) {
+            cin >> arr[i][j];
+            wx[i] = max(wx[i], arr[i][j]);
         }
-        printf("Case #%d: %d\n", ++cnt, ans.front() + 1);
     }
+
+    rep(i, 1, n + 1) {
+        printf("wx[%d] = %d\n", i, wx[i]);
+    }
+
+    km();
+
+    cout << ans << endl;
+
 
     return 0;
 }
--- Problem/C.cpp
+++ Problem/C.cpp
@@ -5,21 +5,13 @@
 
 #define IO ios_base::sync_with_stdio(0),cin.tie(0)
 #define rep(i, a, n) for (int i = a; i < n; i++)
+#define mm(arr, val) memset(arr, val, sizeof(arr))
 
 using namespace std;
-typedef unsigned long long ULL;
-unsigned x, y, z;
+typedef long long LL;
 
-inline unsigned tang() {
-    unsigned t;
-    x ^= x << 16;
-    x ^= x >> 5;
-    x ^= x << 1;
-    t = x;
-    x = y;
-    y = z;
-    z = t ^ x ^ y;
-    return z;
+bool cmp(LL a, LL b) {
+    return a > b;
 }
 
 int main() {
@@ -28,35 +20,93 @@ int main() {
     freopen(R"(C:\Users\ACM-PC\CLionProjects\Competitaon\Problem\in)", "r", stdin);
     freopen(R"(C:\Users\ACM-PC\CLionProjects\Competitaon\Problem\out)", "w", stdout);
 #endif
-    int T, cnt = 0;
-    scanf("%d", &T);
+    LL T;
+    cin >> T;
     while (T--) {
-        int n;
-        scanf("%d %u %u %u", &n, &x, &y, &z);
-        set<ULL> s;
-        ULL ans = 0, tmp;
+        LL x, k, a[100], b[100], c[100];
+        mm(a, 0), mm(b, 0), mm(c, 0);
+        cin >> x >> k;
+        stack<LL> s;
+        set<LL> res;
+        res.insert(x);
+        while (x) {
+            s.push(x % 10);
+            x /= 10;
+        }
+        LL n = 0;
+        while (!s.empty()) {
+            LL x = s.top();
+            s.pop();
+            a[n] = b[n] = c[n] = x;
+            n++;
+        }
+
+        sort(a, a + n, cmp);
+        LL ans = 0, flag = 0;
+
         rep(i, 0, n) {
-            tmp = tang();
-            if (tmp > *s.begin()) {
-                s.insert(tmp);
-                if (s.size() >= 100) {
-                    s.erase(s.begin());
+            if (b[i] != a[i]) {
+                for (LL j = n - 1; j >= 0; j--) {
+                    if (b[j] == a[i]) {
+                        swap(b[j], b[i]);
+                        LL tmp = 0;
+                        rep(p, 0, n) {
+                            tmp = tmp * 10 + b[p];
+                        }
+                        res.insert(tmp);
+                        if (++ans == k) {
+                            flag = 1;
+                        }
+                        break;
+                    }
                 }
             }
+            if (flag) break;
         }
 
-        auto iter2 = s.end();
-
-        for (auto iter = s.begin(); iter != iter2; iter++) {
-            ULL x = *iter;
-            auto iter3 = s.begin();
-            for (; iter3 != iter2 && *iter != *iter3; iter3++) {
-                ULL y = *iter3;
-                if (x != y && __gcd(x, y) == 1) ans = max(ans, x * y);
+        sort(a, a + n);
+        rep(i, 0, n) {
+            if (a[i]) {
+                flag = i;
+                break;
             }
         }
+        for (LL i = flag - 1; i >= 0; i--) {
+            swap(a[i], a[i + 1]);
+        }
+        LL ans2 = 0;
+        flag = 0;
 
-        printf("Case #%d: %llu\n", ++cnt, ans);
+        rep(i, 0, n) {
+            if (c[i] != a[i]) {
+                for (LL j = n - 1; j >= 0; j--) {
+                    if (c[j] == a[i]) {
+                        swap(c[j], c[i]);
+                        LL tmp = 0;
+                        rep(p, 0, n) {
+                            tmp = tmp * 10 + c[p];
+                        }
+                        res.insert(tmp);
+                        if (++ans2 == k) {
+                            flag = 1;
+                        }
+                        break;
+                    }
+                }
+            }
+            if (flag) break;
+        }
+
+//        rep(i, 0, n) {
+//            cout << c[i];
+//        }
+//        cout << " ";
+//        rep(i, 0, n) {
+//            cout << b[i];
+//        }
+//        cout << endl;
+
+        cout << *res.begin() << " " << *(--res.end()) << endl;
     }
     return 0;
 }
